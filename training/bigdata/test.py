7831496875db3f599f6c92e5cd9e81dbac5f8ade
HyeonMyeong Jang <gkagm2@gmail.com>
2018-04-02 11:32:40 +0900

    renamed

---
+++ training/bigdata/test.py
@@ -0,0 +1,25 @@
+import json
+from collections import defaultdict
+
+
+path = 'file.txt'
+records = [json.loads(line) for line in open(path, encoding='UTF-8')]
+time_zones = [rec['tz'] for rec in records if 'tz' in rec]
+
+def get_counts(sequence):
+    counts = {}
+    for x in sequence:
+        if x in counts:
+            counts[x] += 1
+        else:
+            counts[x] = 1
+    return counts
+
+def get_counts2(sequence):
+    counts = defaultdict(int) # values will initialize to 0
+    for x in sequence:
+        counts[x] += 1
+    return counts
+
+counts = get_counts(time_zones)
+print(counts[counts])
