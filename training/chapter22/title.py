d09bdd21c80fd3ac57f76d966d4bb1758ec81865
Sagacity Jang <gkagm2@gmail.com>
2018-05-14 21:39:16 +0900

    title update

---
+++ training/chapter22/title.py
@@ -0,0 +1,8 @@
+# title function
+# This method returns a copy of the string in which first chracters of all the words are capitalized.
+
+
+#example
+string = "hello world nice to meet you!"
+
+print(string.title())
